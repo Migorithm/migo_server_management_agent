ac3f6dbd133c0849e420af0742a704536e2cd0c5
Migorithm <saka90030@gmail.com>
2022-03-16 18:12:47 +0900

    Decorator for token check, config modification logic added

--- agent.py
+++ agent.py
@@ -1,20 +1,27 @@
-from flask import Flask,request
+from flask import Flask,request,make_response
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from utils import AgentUtils
 import os
-
+from functools import wraps
+import yaml
 
 app = Flask(__name__)
 AgentUtils.load_service()
 
 @app.route("/command/restart",methods=["POST"])
 def restart():
-    #confirmation
-    if request.method =="POST":
-        #On the other side, it will post things in the following form
-        # requests.post("http://nodeip:port/command/restart",json={"token":token})
-        byte_token = request.get_json()["token"]
-        token = AgentUtils.token_loader(byte_token)
-        if token.get("confirm"):
-            print(AgentUtils.ELASTICSEARCH_SERVICE.ActiveState)
-            
+    AgentUtils.ELASTICSEARCH_SERVICE.Restart("replace")
+    return "Executed", 200
+
+@app.route("/command/configuration",methods=["POST"])
+@AgentUtils.token_check
+def config() -> tuple:
+    try:
+        with open(os.getenv("FILEPATH"),"w",encoding="UTF-8") as file:
+            res:dict = request.get_json().get("data")
+            yaml.dump(res,file) #put res into file 
+            return "Executed",200 #Status Code 200
+    except Exception as e:
+        return "Failed",400
+
+
--- utils.py
+++ utils.py
@@ -1,14 +1,13 @@
 import os
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-
+from flask import request,jsonify
+from functools import wraps
 
 class AgentUtils:
     AGENT_KEY=os.getenv("AGENT_KEY")
-    
     #Service loading
     def __new__(cls):
         return cls
-    
     @classmethod
     def load_service(cls):
         from pystemd.systemd1 import Unit,manager
@@ -25,18 +24,41 @@ class AgentUtils:
             unit=Unit(service[0].decode("utf-8"))
             unit.load()
             unit=unit.Unit
-            setattr(Config,service_name,unit)
+            setattr(AgentUtils,service_name,unit)
             #Put unit first so you can get the state of unit synchronously.
-    
     @staticmethod
     def token_loader(token):
-        serializer = Serializer(Config.AGENT_KEY)
+        serializer = Serializer(AgentUtils.AGENT_KEY)
         return serializer.loads(token.encode("utf-8"))
-            
     
-
+    @staticmethod
+    def token_check(func):
+        @wraps(func)
+        def wrapper(*args,**kwagrs):
+            #confirmation
+            if request.method =="POST" : #request.remote_addr 
+                #On the other side, it will post things in the following form
+                #requests.post("http://nodeip:port/command/restart",json={"token":token})
+                byte_token = request.get_json().get("token")
+                if byte_token:
+                    try:
+                        token= AgentUtils.token_loader(byte_token) #Error point
+                        if token.get("confirm"):
+                            return func(*args,**kwagrs)
+                        else:
+                            print("Token not matched")
+                            return "Failed", 400
+                    except Exception as e:
+                        print("BadSignature!")
+                        print(e)
+                        return "Failed",400   
+                else:
+                    print("Token not given!")
+                    return "Failed",400
+        return wrapper
         
-                
+            
+                    
 
     
         
