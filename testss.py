1f39703a8fe8ecd69b0df1c5e72fd14123c82ff9
Migorithm <saka90030@gmail.com>
2022-03-30 08:50:00 +0900

    up to date

--- testss.py
+++ testss.py
@@ -6,8 +6,7 @@ class age:
   
     def __new__(cls):
         return cls
-            
-    #Automatic registration of services
+    
     @classmethod
     def LoadService(cls):
         interface = SystemdManager._get_interface()
@@ -19,15 +18,42 @@ class age:
             class kls:
                 @staticmethod
                 def Restart(unit=unit,mode=b"replace"):
-                    SystemdManager.Restart(unit,mode)
+                    res = SystemdManager.Restart(unit,mode)
+                    return res
                 @staticmethod
                 def Start(unit=unit,mode=b"replace"):
-                    SystemdManager.Start(unit,mode)
+                    res = SystemdManager.Start(unit,mode)
+                    return res
                 @staticmethod
                 def Stop(unit=unit,mode=b"replace"):
-                    SystemdManager.Stop(unit,mode)
+                    res = SystemdManager.Stop(unit,mode)
+                    return res
             setattr(cls,service_name,kls)
-            
+    
+    
+    #Automatic registration of services
+    # @classmethod
+    # def LoadService(cls):
+    #     interface = SystemdManager._get_interface()
+    #     regex = re.compile(r"redis.*service|elastic.*service|kafka.*service|vertica.*service")
+    #     unitnames = [str(unit[0]) for unit in interface.ListUnits() if regex.match(unit[0])]
+        
+    #     for unit in unitnames:
+    #         service_name = re.sub(r"[@.-]",r"_",unit).upper()
+    #         class kls:
+    #             @staticmethod
+    #             def Restart(unit=unit,mode=b"replace"):
+    #                 SystemdManager.Restart(unit,mode)
+    #                 return True
+    #             @staticmethod
+    #             def Start(unit=unit,mode=b"replace"):
+    #                 SystemdManager.Start(unit,mode)
+    #                 return True
+    #             @staticmethod
+    #             def Stop(unit=unit,mode=b"replace"):
+    #                 SystemdManager.Stop(unit,mode)
+    #                 return True
+    #         setattr(cls,service_name,kls)
       
 
     
